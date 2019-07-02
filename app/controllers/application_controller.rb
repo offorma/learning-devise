d54a5dab9e7a61d97054f2d54a105d8b6b8d9e3d
offorma <offorma@gmail.com>
2019-07-02 13:53:35 +0100

    modified registration form to require new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:email,:phone)}
+            devise_parameter_sanitizer.permit(:sign_up) {|u|
+            u.permit(:email, :firstname, :lastname, :phone, :password, :password_confirmation)}
+        end
     
 end
